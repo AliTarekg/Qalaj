6121a4a16b25bedd5070fd7ecb0dd0c42d492d88
Ali <3tarek@protonmail.com>
2025-05-03 03:52:25 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Qalag/', // Set base for GitHub Pages deployment
+  base: '/qalag/', // Set base for GitHub Pages deployment
   plugins: [react()],
 })
